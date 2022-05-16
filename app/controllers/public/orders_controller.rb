6fd0cf0d4f4b0ac0719006b79b925986f644de9b
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-16 12:39:25 +0000

    [Add]enum導入

---
+++ app/controllers/public/orders_controller.rb
@@ -0,0 +1,20 @@
+class Public::OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    
+  end
+  
+  def create
+  end
+
+  def finish
+  end
+
+  def confirm
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,9 @@ class Order < ApplicationRecord
 
   belongs_to :customer
   has_many :order_details, dependent: :destroy
-
+  
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
+
+# 0 が入っている場合は credit_card(クレジットカード)
+# 1 が入ってる場合は transfer(銀行振込)
